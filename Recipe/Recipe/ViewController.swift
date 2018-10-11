57a966e4ce9cafccc51097d86c1342406e549708
Caitlin Jones <cnjo224@g.uky.edu>
2018-10-11 16:44:05 -0400

    Init of project file

---
+++ Recipe/Recipe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Recipe
+//
+//  Created by Jones, Caitlin on 10/11/18.
+//  Copyright © 2018 Jones, Caitlin All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
