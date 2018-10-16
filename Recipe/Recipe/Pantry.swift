0ac5e0d75f933eeee625be0052f0299c477fe4bd
Adam Brassfield <adam.brassfield@uky.edu>
2018-10-16 15:46:42 -0400

    Added Pantry.swift & Recipe.swift

---
+++ Recipe/Recipe/Pantry.swift
@@ -0,0 +1,9 @@
+//
+//  Pantry.swift
+//  Recipe
+//
+//  Created by Adam Brassfield on 10/16/18.
+//  Copyright © 2018 Jones, Caitlin N. All rights reserved.
+//
+
+import Foundation
---
+++ Recipe/Recipe/Recipe.swift
@@ -0,0 +1,16 @@
+//
+//  Recipe.swift
+//  Recipe
+//
+//  Created by Adam Brassfield on 10/16/18.
+//  Copyright © 2018 Jones, Caitlin N. All rights reserved.
+//
+
+import Foundation
+
+struct Recipe {
+    var name: String
+    var author: [String]
+    var ingredients: [String]
+    var instructions: [String]
+}
