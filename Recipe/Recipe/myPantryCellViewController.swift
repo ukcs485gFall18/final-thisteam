a71cfb0ca194c6b39669d6691a2f6ceab4352547
Ryan Gaines <ryan.gaines@uky.edu>
2018-10-28 21:34:23 -0400

    populates pantry list

---
+++ Recipe/Recipe/myPantryCellViewController.swift
@@ -0,0 +1,17 @@
+//
+//  myPantryCellViewController.swift
+//  Recipe
+//
+//  Created by Ryan Gaines on 10/28/18.
+//  Copyright © 2018 Jones, Caitlin N. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class myPantryCell : UITableViewCell{
+    
+    @IBOutlet weak var cellLabel:UILabel!
+    
+    
+}
---
+++ Recipe/Recipe/viewmyPantryViewController.swift
@@ -0,0 +1,31 @@
+//
+//  viewmyPantryViewController.swift
+//  Recipe
+//
+//  Created by Ryan Gaines on 10/28/18.
+//  Copyright © 2018 Jones, Caitlin N. All rights reserved.
+//
+// based off video https://www.youtube.com/watch?v=-tRQmVkrfHc
+
+
+import Foundation
+import UIKit
+
+class viewmyPantryViewController : UITableViewController{
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pantry.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "pantryCell") as! myPantryCell
+        
+        cell.cellLabel.text = pantry[indexPath.row].name
+        return cell
+    }
+    
+}
