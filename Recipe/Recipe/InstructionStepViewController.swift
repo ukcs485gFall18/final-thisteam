2787c4a404279a5adf186f1eb1324c980dd9353d
Caitlin Jones <cnjo224@g.uky.edu>
2018-11-04 23:48:33 -0500

    Set up UI for viewing recipe and viewing steps. Not finished with implementation of custom classes for UI. Can progress to view recipe controller.

---
+++ Recipe/Recipe/InstructionStepViewController.swift
@@ -0,0 +1,19 @@
+//
+//  InstructionStepViewController.swift
+//  Recipe
+//
+//  Created by Jones, Caitlin on 11/4/18.
+//  Copyright © 2018 Jones, Caitlin N. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class InstructionStepViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("got to step by step instruction")
+    }
+    
+}
---
+++ Recipe/Recipe/ViewRecipeViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewRecipeViewController.swift
+//  Recipe
+//
+//  Created by Jones, Caitlin on 11/4/18.
+//  Copyright © 2018 Jones, Caitlin N. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ViewRecipeViewController: UIViewController{
+    @IBOutlet weak var RecipeImage: UIImageView!
+    
+    @IBOutlet weak var IngredientCell: UITableView!
+    
+    @IBOutlet weak var StepCell: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    
+    }
+    
+    
+}
