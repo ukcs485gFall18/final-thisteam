c29706c4620ffdb7367974ec6876204011691369
Caitlin Jones <cnjo224@g.uky.edu>
2018-12-01 19:10:09 -0500

    Transferred selected cell (recipe name only) from CookbookTableViewController to ViewRecipeViewController. Populated the navigation header for ViewRecipe with name. Working on scrolling.

--- Recipe/Recipe/ViewRecipeViewController.swift
+++ Recipe/Recipe/ViewRecipeViewController.swift
@@ -4,23 +4,28 @@
 //
 //  Created by Jones, Caitlin on 11/4/18.
 //  Copyright © 2018 Jones, Caitlin N. All rights reserved.
-//
+// https://stackoverflow.com/questions/25167458/changing-navigation-title-programmatically
+
 
 import Foundation
 import UIKit
 
 class ViewRecipeViewController: UIViewController{
     @IBOutlet weak var RecipeImage: UIImageView!
-    
-    @IBOutlet weak var IngredientCell: UITableView!
+
+    @IBOutlet weak var Ingredients: UITextView!
     
     @IBOutlet weak var StepCell: UITableView!
+    @IBOutlet weak var Label: UILabel?
+    
+    //needed to recieve recipe name from CookBookTableViewController
+    var recipeName:String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        //set the recipe name in the viewController's navigation bar
+        self.title = recipeName
         
-        
-    
     }
     
     
