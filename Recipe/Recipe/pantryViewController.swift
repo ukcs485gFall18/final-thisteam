8abd0b3bb8c3a82b24b04ab8e84c78633cb9bbe4
Ryan Gaines <ryan.gaines@uky.edu>
2018-12-08 16:44:25 -0500

    work on pantry view

--- Recipe/Recipe/pantryViewController.swift
+++ Recipe/Recipe/pantryViewController.swift
@@ -7,20 +7,36 @@
 //https://stackoverflow.com/questions/40484182/ios-swift-3-uidatepicker
 //https://stackoverflow.com/questions/43148864/how-to-get-a-push-notification-at-a-set-time-swift-3
 
+
 import Foundation
 import UIKit
 
-class pantryViewController : UIViewController{
+class pantryViewController : UIViewController, UIPickerViewDelegate, UIPickerViewDataSource{
     
-    @IBOutlet weak var pantryQuantity:UITextField!
-    @IBOutlet weak var pantryMeasuringAmount:UITextField!
+    
+    @IBOutlet weak var pantryQuantity:UIPickerView!
     @IBOutlet weak var pantryItem:UITextField!
+    
+    // https://en.wikipedia.org/wiki/Cooking_weights_and_measures
+    private var pantryData:[[String]] = [[String]]()
     
     @IBOutlet weak var expirationLabel: UILabel!
     @IBOutlet weak var expireDate: UIDatePicker!
     override func viewDidLoad() {
         super.viewDidLoad()
         expireDate.datePickerMode = UIDatePicker.Mode.date
+        let arr = Array(0...99)
+        var arrStr:[String] = [String]()
+        for i in arr{
+            arrStr.append(String(i))
+        }
+        self.pantryData = [arrStr,
+                           ["."],
+                           arrStr,
+                           ["Gallon", "Liter", "Quart", "Pint",
+                            "Cup", "Fluid Ounce", "Tablespoon", "Teaspoon"]
+            ]
+        self.setupPicks()
         
     }
     
@@ -32,7 +48,25 @@ class pantryViewController : UIViewController{
         }
         
     }
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 4
+    }
     
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pantryData[component].count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pantryData[component][row]
+    }
+
+
+    func setupPicks(){
+        self.pantryQuantity.delegate = self
+        self.pantryQuantity.dataSource = self
+        
+    }
     
     func prepareForNotfication(){
         
