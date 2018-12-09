bd8a6c09a49094fdc094d65f8ee60ff775536aef
Ryan Gaines <ryan.gaines@uky.edu>
2018-12-09 18:11:42 -0500

    worked on view & view controller for pantry contents

---
+++ Recipe/Recipe/pantryTableCell.swift
@@ -0,0 +1,16 @@
+//
+//  pantryTableCell.swift
+//  Recipe
+//
+//  Created by Ryan Gaines on 12/9/18.
+//  Copyright © 2018 Jones, Caitlin N. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class pantryCell: UITableViewCell{
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var amount: UILabel!
+    
+}
--- Recipe/Recipe/viewPantryController.swift
+++ Recipe/Recipe/viewPantryController.swift
@@ -11,15 +11,42 @@
 import Foundation
 import UIKit
 
-class viewPantryContentsController: UIViewController{
+class viewPantryContentsController: UIViewController, UITableViewDataSource,  UITableViewDelegate, UISearchBarDelegate{
+
+    
     var model:pantryModel = pantryModel()
     
+    //search bar: https://shrikar.com/swift-ios-tutorial-uisearchbar-and-uisearchbardelegate/
     @IBOutlet weak var search: UISearchBar!
     @IBOutlet weak var myTable: UITableView!
+    var isSearching:Bool = false
+    var filteredData:[Ingredient] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.myTable.dataSource = self
+        self.myTable.delegate = self
+        self.search.delegate = self
+        self.myTable.rowHeight = 80.0     //https://stackoverflow.com/questions/25632394/swift-uitableview-set-rowheight
         
+        self.myTable.reloadData()
+        
+    }
+    
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        isSearching = true;
+    }
+    
+    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
+        isSearching = false;
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        isSearching = false;
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        isSearching = false;
     }
     
     @IBAction func sortData(_ sender: Any) {
@@ -41,6 +68,44 @@ class viewPantryContentsController: UIViewController{
         sortAlert.addAction(sortByExpire)
         sortAlert.addAction(cancel)
         self.present(sortAlert, animated: true, completion: nil)
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return model.pantry.count
+        
+    }
+    
+    func convertToStr(measure: MeasurementUnits?) ->String{
+        switch measure {
+        case .cup?:
+            return "cup"
+        case .gal?:
+            return "gallon"
+        case .lbs?:
+            return "pound"
+        case .oz?:
+            return "ounce"
+        case .tbsp?:
+            return "tablespoon"
+        case .tsp?:
+            return "teaspoon"
+        default:
+            return "unit"
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = myTable.dequeueReusableCell(withIdentifier: "myPantryCell") as! pantryCell
+        let name = model.pantry[indexPath.row].name
+        let amount = model.pantry[indexPath.row].quantity
+        let measure = model.pantry[indexPath.row].units
+        var measureStr = convertToStr(measure: measure)
+        if amount != 1.0{
+            measureStr += "s"
+        }
+        cell.name.text = name
+        cell.amount.text = String(amount) + " " + measureStr
+        return cell
+        
     }
     
 }
