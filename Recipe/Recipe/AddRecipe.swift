61f25ba180c343ed62c969f639c8fec92d25f646
Ryan Gaines <ryan.gaines@uky.edu>
2018-12-12 03:45:50 -0500

    start work on adding recipes

--- Recipe/Recipe/AddRecipe.swift
+++ Recipe/Recipe/AddRecipe.swift
@@ -9,7 +9,9 @@
 import Foundation
 import UIKit
 
-class AddRecipeViewController: UIViewController{
+class AddRecipeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
+
+    
     
     @IBOutlet weak var RecipeImage: UIImageView!
     @IBOutlet weak var ChangeImage: UIButton!
@@ -21,10 +23,29 @@ class AddRecipeViewController: UIViewController{
     @IBOutlet weak var InstructionTable: UITableView!
     
     @IBOutlet weak var AddIngredient: UIButton!
+    
+    var instructions:[String] = [String]()
+    var ingredients:[Ingredient] = [Ingredient]()
+    
     @IBAction func AddIngredient(_ sender: Any) {
         //Popup a box to add ingredient
         let alertController = UIAlertController(title: "Add Ingredients", message: "", preferredStyle: .alert)
-        
+        alertController.addTextField(configurationHandler: {(textField) in
+            textField.placeholder = "Ingredient Name..."
+        })
+        alertController.addTextField(configurationHandler: {(textField) in
+            textField.placeholder = "Amount..."
+        })
+        alertController.addTextField(configurationHandler: {(textField) in
+            textField.placeholder = "Measure..."
+        })
+        let saveAction = UIAlertAction(title: "Save", style: .default, handler: {alert->Void in
+            print("SAVE")
+        })
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alertController.addAction(saveAction)
+        alertController.addAction(cancelAction)
+        self.present(alertController, animated: true, completion: nil)
 
     }
     
@@ -32,6 +53,19 @@ class AddRecipeViewController: UIViewController{
     @IBOutlet weak var AddInstruction: UIButton!
     @IBAction func AddInstruction(_ sender: Any) {
         //Popup a box to add Instruction
+        let alertController = UIAlertController(title: "Add Instructions!", message: "", preferredStyle: .alert)
+        alertController.addTextField(configurationHandler: { (textField) in
+            textField.placeholder = "Enter step here..."
+        })
+        let saveAction = UIAlertAction(title: "Save", style: .default, handler: {alert->Void in
+            let curr = alertController.textFields![0] as UITextField
+            self.instructions.append(curr.text!)
+            self.InstructionTable.reloadData()
+        })
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alertController.addAction(saveAction)
+        alertController.addAction(cancelAction)
+        self.present(alertController, animated: true, completion: nil)
     }
     
     
@@ -42,44 +76,41 @@ class AddRecipeViewController: UIViewController{
         
         if(nameFromSegue != ""){
             NameEdit.text = nameFromSegue
-            /* Update fields with information
-            RecipeImage =
-            PrepTimeEdit.text =
-             CookTimeEdit.text =
-             TempEdit.text =
-             
-             
-            */
-            
         }
         
-        /*
         self.IngredientTable.delegate = self
         self.IngredientTable.dataSource = self
+        self.IngredientTable.reloadData()
+        self.IngredientTable.accessibilityIdentifier = "ingred"
         
         self.InstructionTable.delegate = self
-        self.InstructionTable.dataSource = self*/
-    }
-    
-    /*func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath)-> UITableViewCell{
-        if( tableView == IngredientTable){
-            let cell = self.IngredientTable.dequeueReusableCell(withIdentifier: "IngredientCell") as! IngredientCell
-            cell.IngredientLabel.text = self.items[indexPath.row]
-            cell.Amount.text = "2 tsp"
-            /* if ingredient in pantry: change image to checkmark
-             cell.InPantryImg
-             */
-            return cell
-        }else{
-            let cell = self.InstructionTable.dequeueReusableCell(withIdentifier: "InstructionCell") as! InstructionCell
-            cell.StepText.text = self.items[indexPath.row]
-            cell.Num.text = String(indexPath.row + 1)
-            return cell
-        }
+        self.InstructionTable.dataSource = self
+        self.InstructionTable.reloadData()
+        self.InstructionTable.accessibilityIdentifier = "instruct"
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
-    }*/
+        if tableView.accessibilityIdentifier == "ingred"{
+            return self.ingredients.count
+        }
+        else{
+            return self.instructions.count
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if tableView.accessibilityIdentifier == "ingred"{
+            let cell = self.IngredientTable.dequeueReusableCell(withIdentifier: "IngredientCell") as! IngredientCell
+            cell.Amount.text = String(self.ingredients[indexPath.row].quantity) + self.ingredients[indexPath.row].units!
+            cell.IngredientLabel.text = self.ingredients[indexPath.row].name!
+            return cell
+        }
+        else{
+            let cell = self.InstructionTable.dequeueReusableCell(withIdentifier: "InstructionCell") as! InstructionCell
+            //cell.Num.text = String(indexPath.row - 1)
+            cell.StepText.text = String(indexPath.row + 1) + ")" + " " + self.instructions[indexPath.row]
+            return cell
+        }
+    }
     
 }
